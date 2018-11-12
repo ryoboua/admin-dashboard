8280ee70f618a86619731c8968bd4ea925838943
Reggie <ryoboua@gmail.com>
2018-11-12 12:21:16 -0500

    Update src/components/EmployeeTable.vue

--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -1,26 +1,39 @@
 <template>
-      <table class="striped highlight responsive-table">
-        <thead>
-          <tr>
-            <th>User</th>
-            <th>Name</th>
-            <th>Designation</th>
-            <th>Status</th>
-            <th>Performance</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="e in employeeList" :key=e.id>
-            <td>{{ e.name }}</td>
-            <td>{{ e.name }}</td>
-            <td>{{ e.designation }}</td>
-            <td>{{ e.status }}</td>
-            <td>{{ e.performance }}</td>
-          </tr>
-        </tbody>
-      </table>
+    <div class="col s12 l8 white table-container">
+        <h5 class="left-align pl-1">Employee List</h5>
+        <div class="divider"></div>
+        <collapse :selected="true">
+            <div slot="collapse-body">
+            <table class="highlight responsive-table centered">
+                <thead>
+                <tr>
+                    <th>User</th>
+                    <th>Name</th>
+                    <th>Designation</th>
+                    <th>Status</th>
+                    <th>Performance</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="e in employeeList" :key=e.id>
+                    <td>{{ e.name }}</td>
+                    <td>{{ e.name }}</td>
+                    <td>{{ e.designation }}</td>
+                    <td>{{ e.status }}</td>
+                    <td>{{ e.performance }}</td>
+                </tr>
+                </tbody>
+            </table>
+            <div slot="collapse-header">
+            </div>
+            </div>
+        </collapse>
+     </div>
 </template>
 <script>
+
+    import Collapse from './Collapse'
+
     export default {
         name: 'EmployeeTable',
         props: {
@@ -37,10 +50,21 @@
                 ]
             },
         },
+        components: {
+            Collapse
+        },
         
     }
 </script>
 <style scoped>
+ .table-container {
+     border-radius: 5px;
+     padding: 1em 0;
+    margin-bottom: 0.5em;
 
+}
+.pl-1 {
+    padding-left: 1.6%;
+}
 </style>
 
