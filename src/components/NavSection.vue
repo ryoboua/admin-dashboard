f74fc89a45eb44ad5f3f950aab2f218c2f65d595
Reggie <ryoboua@gmail.com>
2018-11-12 23:53:57 -0500

    Update src/components/NavSection.vue

--- src/components/NavSection.vue
+++ src/components/NavSection.vue
@@ -1,18 +1,34 @@
 <template>
-    <div class="nav-section-container grey lighten-3" >
+    <div>
+      <nav>
+        <div class="nav-wrapper indigo">
+        <form>
+            <div class="input-field">
+            <input id="search" type="search" required>
+            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+            </div>
+        </form>
+        </div>
+  </nav>
+    <div class="nav-section-container grey lighten-3">
       <div class="nav-wrapper black-text">
         <div class="left-align black-text">
           <a href="#!" class="breadcrumb black-text">
             <i class="small material-icons home-icon">home</i>
-            Home
+            <span class="hide-on-small-only">Home</span>
           </a>
-          <a href="#!" class="breadcrumb black-text">Dashboard</a>
+          <a href="#!" class="breadcrumb black-text hide-on-small-only"><strong>Dashboard</strong></a>
         </div>
       </div>
-      <div class="black-text admin-total-wrapper">
-            <p class="mr"> ${{ totalIncome }} Total Income</p>
-            <p class="mr"> {{ newUsers }} New Users</p>
+        <div class="black-text admin-total-wrapper">
+            <p class="mr"> <i class="material-icons">notifications</i><span class="new badge red white-text">{{notification}}</span></p>
+            <p class="mr"> <i class="material-icons">message</i><span class="new badge red white-text">{{messages}}</span></p>
+            <p class="mr"> <i class="material-icons">settings</i></p>
+
+            <br />
         </div>
+    </div>
     </div>
 </template>
 
@@ -27,14 +43,6 @@ export default {
         imgURL: {
             type: String,
             default: 'Default Name'
-        },
-        totalIncome: {
-                type: Number,
-                default: 34,
-        },
-        newUsers: {
-            type: Number,
-            default: 20
         },
         notification: {
             type: Number,
@@ -65,7 +73,10 @@ export default {
     .admin-total-wrapper {
         line-height: 0px;
         display: flex;
-        margin: 13px 1em 0px auto
+        margin: 0px 1em 0px auto;
+        width: auto;
+        flex-wrap: wrap;
+        justify-content: flex-end;
     }
     .mr {
         margin-right: 1em;
